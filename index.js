adce226ad7301e945d601b5bf0f09e944aa81748
Jsheumaker1 <Jsheumaker1@gmail.com>
2021-05-05 21:00:56 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+function saturdayFun (activity = `roller-skate`){
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+saturdayFun("bathe my dog")
+
+const mondayWork = function (work= 'go to the office'){
+    return(`This Monday, I will ${work}.`)
+}
+
+mondayWork("work from home")
+
+function wrapAdjective(result = '*'){
+    return function (emphatic = 'special') {
+        return (`You are ${result}${emphatic}${result}!`) 
+    }
+} 
+
+wrapAdjective('*')("a hard worker")
+wrapAdjective('||')("a dedicated programmer")
